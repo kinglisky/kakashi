f76ab2d91f31e674b2724a7607bd99ff52434821
芊野 <free.nan.sky@gmail.com>
2022-01-10 23:56:49 +0800

    feat: 文件下载

--- get-resources.js
+++ get-resources.js
@@ -1,19 +1,38 @@
 const Crawler = require('crawler');
+const download = require('download');
 
-const c = new Crawler({});
+const fetchImages = () => {
+    return new Promise((resolve, reject) => {
+        const c = new Crawler({});
+        c.queue([{
+            jQuery: true,
+            uri: 'http://jandan.net/top',
+            callback: (error, res, done) => {
+                if (error) {
+                    done();
+                    return reject(error);
+                }
+                done();
+                const images = res
+                    .$('#comments ol li .text .view_img_link')
+                    .map((_, it) => `https:${it.attribs.href}`)
+                    .get();
+                resolve(images);
+            }
+        }]);
+    });
+}
 
-// Queue URLs with custom callbacks & parameters
-c.queue([{
-    jQuery: true,
-    uri: 'http://jandan.net/top',
-    // The global callback won't be called
-    callback: (error, res, done) => {
-        if (error) {
-            console.log(error);
-        } else {
-            const images = res.$('#comments ol li .text img').map((index, it) => it.attribs.src).get();
-            console.log(images);
-        }
-        done();
-    }
-}]);
+const downloadImages = (images) => {
+    const tasks = images.map((image, index) => {
+        console.log('download', image);
+        const filename = `${index}${image.match(/\.\w+$/)[0]}`
+        return download(image, 'download', { filename });
+    });
+    return Promise.all(tasks);
+}
+
+(async function () {
+    const images = await fetchImages();
+    await downloadImages(images);
+})();
