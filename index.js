ade3d579727257c0a6b45d3c20c24e4799baa2c2
kinglisky <kinglisky@icloud.com>
2022-01-18 00:48:37 +0800

    feat: update

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const { fetchImages, downloadImages } = require('./resources');
+const fs = require('fs');
+const uitl = require('util');
+const { fetchComments, downloadComments } = require('./resources');
+const writeFile = uitl.promisify(fs.writeFile);
 
 (async function () {
-    const images = await fetchImages();
-    const paths = await downloadImages(images);
-    console.log(paths);
+    const comments = await fetchComments();
+    const res = await downloadComments(Array.from(comments));
+    await writeFile('./res.json', JSON.stringify(res, null, 4));
+    console.log(res);
 })();
