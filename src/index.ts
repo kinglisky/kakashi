a0da642fad2169c50893d4e4ee508c93d6f1788d
kinglisky <kinglisky@icloud.com>
2022-01-19 01:00:34 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import { getImageMeta } from './convert';
 (async function name() {
     const testFilePath = './download/2022118/001Hf62Dly1gyft1n5wc1g60a00a0u1902.gif';
     const res = await getImageMeta(testFilePath);
-    debugger;
-    console.log(res);
+    console.log('xx', res);
 })();
