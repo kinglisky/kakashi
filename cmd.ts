4397c5f3ceb9b6191056707c7b9d338435369550
qianye <qianye@gaoding.com>
2022-03-18 18:44:07 +0800

    feat: 基本完成

--- cmd.ts
+++
@@ -1,7 +0,0 @@
-import ffmpeg from 'fluent-ffmpeg';
-
-(async function () {
-    ffmpeg.ffprobe('./output.mp4', function (err, metadata) {
-        console.dir(metadata);
-    });
-})();
--- dev.ts
+++ dev.ts
@@ -11,21 +11,23 @@ import { createViode } from './src/index';
 (async function () {
     const comments = await fetchComments();
     console.log(comments);
-    const items = await downloadComments(comments.slice(2, 12));
+    const items = await downloadComments(comments.slice(4, 10));
     const res = await convertImages(items, {
-        width: 1920,
+        width: 750,
         height: 1080,
     });
     console.log('done', res);
-    createViode(res, {
-        width: 1920,
-        height: 1080,
-        output: 'output.mp4',
-        outputDir: path.join(__dirname, './output'),
-        cacheDir: path.join(__dirname, './cache'),
-        fps: 60,
-        audio: {
-            path: path.join(__dirname, './assets/test.mp3'),
-        },
-    });
+    setTimeout(() => {
+        createViode(res, {
+            width: 750,
+            height: 1080,
+            output: 'output.mp4',
+            outputDir: path.join(__dirname, './output'),
+            cacheDir: path.join(__dirname, './cache'),
+            fps: 60,
+            audio: {
+                path: path.join(__dirname, './assets/test.mp3'),
+            },
+        });
+    }, 400);
 })();
